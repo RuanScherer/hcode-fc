c46d4b18a3c620c99aa1b1c43631d401906dc104
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-05-08 15:37:47 -0300

    Header created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import AppHeader from './components/AppHeader'
+  export default {
+    name: 'App',
+    components: {
+      AppHeader
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Rajdhani&display=swap');
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,52 @@
+<template>
+	<header>
+		<div class="container">
+			<nav class="navbar navbar-expand">
+				<a href="#" class="link navbar-brand mr-auto">
+					<img 
+						src="./../assets/logo.svg" 
+						alt="Hcode Treinamentos"
+						class="img d-inline-block align-top"
+						id="logo">
+					Futebol Clube
+				</a>
+				<div class="links mr-2">
+					<a href="#" class="btn mr-5">Início</a>
+					<a href="#" class="btn mr-5">Times</a>
+					<a href="#" class="btn mr-5">Classificações</a>
+				</div>
+			</nav>
+		</div>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: 'name'
+	}
+</script>
+
+<style scoped>
+	header {
+		background-color: #111;
+	}
+	#logo {
+		width: 150px;
+		height: 100%;
+	}
+	.links {
+		display: inline-flex;
+	}
+	.link {
+		color: #D48E23;
+		font-size: 30px;
+		font-family: 'Rajdhani'
+	}
+	.links a {
+		color: #fff;
+	}
+	.links a:focus {
+		outline: none !important;
+		box-shadow: none
+	}
+</style>
